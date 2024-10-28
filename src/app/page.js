a9d985c2e400f05454824d0b1e532ca976750cc7
Anssi Siren <pro@avaruus.mozmail.com>
2024-10-28 20:03:17 +0200

    Conditional tables

--- src/app/page.js
+++ src/app/page.js
@@ -54,9 +54,9 @@ export default async function Home() {
     <div className="font-[family-name:var(--font-geist-sans)] p-4">
       <main className="flex flex-col gap-8 row-start-2 items-center">
         <h1 className="text-2xl text-center">Electricity prices {updatedAt}</h1>
-        {dataYesterday && <PricesTable {...dataYesterday} />}
         {dataToday && <PricesTable {...dataToday} />}
         {dataTomorrow && <PricesTable {...dataTomorrow} />}
+        {!dataTomorrow && dataYesterday && <PricesTable {...dataYesterday} />}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
       <ScrollIntoView />
